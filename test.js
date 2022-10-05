df9518f10cb1b3e1d5fc61de541c270d77a3fbc4
srushtikorane45 <koranesrushti4@gmail.com>
2022-10-05 12:40:59 +0530

    Added Console Methods
    Added Console Methods

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 console.log("Hello");
+console.info("My name is Srushti Patil")
+console.info("My Hobbies: 1.Read the novels 2.cooking 3.Drawing")
+console.info("My Strengths: 1.Easily adapt new things")
+console.warn("My Weaknesses: 1.Emotional 2.Communication Skill")
+console.log(30+23)
+console.log(346*688)
