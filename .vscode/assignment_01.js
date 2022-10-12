2292bbad76a8e73b9223603e849a70a4c287aed8
srushtikorane45 <koranesrushti4@gmail.com>
2022-10-12 20:41:07 +0530

    String Methods
    String Methods

---
+++ .vscode/assignment_01.js
@@ -0,0 +1,26 @@
+var st="My dream company is TCS.";
+console.log(st);
+console.log("-----------My hobbies are-------------");
+var hobby1= "Reading books ";
+var hobby2= "Dancing ";
+var hobby3= "Listening music "
+console.log(hobby1);
+console.log(hobby2);
+console.log(hobby3);
+var st= hobby1.concat( hobby2);
+console.log("-------------after concat--------------");
+console.log(st.concat( hobby3));
+console.log("---------------------------------------");
+var stringHandsOn="  Hey you are doing good, keep it up  ";
+console.log("1.",stringHandsOn);
+var len=stringHandsOn.length;
+console.log("2. Length of string:",len);
+console.log("3. Before removing extra spaces:",stringHandsOn.length);
+var space=stringHandsOn.trim();
+console.log("   After removing extra spaces:",space.length);
+console.log("4. Total number of extra spaces:",stringHandsOn.length-space.length);
+var ch=stringHandsOn.indexOf(0);
+console.log(ch);
+
+
+
---
+++ .vscode/assignment_02.js
@@ -0,0 +1,7 @@
+var string="  Hey you are doing good, keep it up  ";
+console.log(string);
+console.log("---------------------------------------");
+var len=string.length;
+console.log(len);
+console.log("----------------------------------------------");var trime=string.trim;
+console.log(string.trim());
