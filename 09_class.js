8a0bf1b5382e0327bdc545c87e209c30b5f51fc9
srushtikorane45 <koranesrushti4@gmail.com>
2022-11-02 08:43:23 +0530

    Class
    Class

---
+++ 09_class.js
@@ -0,0 +1,17 @@
+class Person{
+    constructor(fullName, city, age, gender){
+      this.fullName =   fullName;
+      this.city = city;
+      this.age = age;
+      this.gender = gender;
+    }
+    
+}
+let sachin = new Person("Sachin Tendulkar", "Pune", 45, "Male");
+let rohit = new Person("Rohit Shram", "Mumbai", 32, "Male");
+console.log(sachin);
+console.log(rohit);
+
+
+
+
