7f84ae9b3213f9f78109b51ff0cb5eb29e0430c2
srushtikorane45 <koranesrushti4@gmail.com>
2022-11-02 08:45:07 +0530

    Map Basics
    Map Basics

---
+++ 13_map_basics.js
@@ -0,0 +1,40 @@
+let map =new map();
+map.set(22,"Sachin");
+map.set(11,"Dravid");
+map.set(44,"Ganguly");
+map.set(55,"Dravid");
+console.log("--------------------------- Adding Duplicate Key--------------------");
+console.log("------ Traversing over map collection --------");
+let setOfKeys = map.keys();
+console.log(setOfKeys);
+for (const key of setOfKeys) {
+    console.log(map.get(key));
+}
+
+class Person {
+    constructor(rollNo, fullName, city, age, gender){
+        this.rollNo = rollNo;
+      this.fullName =   fullName;
+      this.city = city;
+      this.age = age;
+      this.gender = gender;
+    }
+    details(){
+        console.log(`Person Details: ${this.rollNo}  ${this.fullName}  ${this.city}  ${this.age}  ${this.gender}`);
+    }
+}
+let personAnil = new Person(22,"Anil Mohite", "Pune", 31, "Male");
+let personAnita = new Person(33, "Anita Golate", "Mumbai", 22, "Female");
+let personBill = new Person(44, "Billgates", "USA", 67, "Male");
+
+let mapOfPersons = new Map();
+mapOfPersons.set(22, personAnil);
+mapOfPersons.set(33, personAnita);
+mapOfPersons.set(44, personBill);
+
+let mapKeys = mapOfPersons.keys(); // 22   33   44
+for (const key of mapKeys) {
+     let personObject = mapOfPersons.get(key);
+     personObject.details();
+
+}
